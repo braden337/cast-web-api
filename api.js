1b9a7d6d13d02aeca37e2c8c3d67b449f0dd17f2
Braden Jonsson <braden337@gmail.com>
2019-12-11 10:33:36 -0600

    Add ability to serve a frontend app

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
-const Express = require('express');
-const bodyParser = require('body-parser');
+const Express = require("express");
+const bodyParser = require("body-parser");
 const configuration = require("./lib/config/config.js");
 
 // console.log('argv:');
@@ -13,41 +13,44 @@ const device = require("./lib/device");
 const config = require("./lib/config");
 const deviceId = require("./lib/device/id");
 const swagger = require("./lib/swagger");
+const frontend = require("./lib/frontend");
 
 startApi();
 
 function startApi() {
-	console.log('cast-web-api v'+configuration.thisVersion);
-	createWebServer();
+  console.log("cast-web-api v" + configuration.thisVersion);
+  createWebServer();
 }
 
 function createWebServer() {
-	const webserver = Express();
-	webserver.use(bodyParser.json());
+  const webserver = Express();
+  webserver.use(bodyParser.json());
 
-	webserver.use(assistant);
-	webserver.use(callback);
-	webserver.use(device);
-	webserver.use(deviceId);
-	webserver.use(config);
-	webserver.use(swagger);
+  webserver.use(assistant);
+  webserver.use(callback);
+  webserver.use(device);
+  webserver.use(deviceId);
+  webserver.use(config);
+  webserver.use(swagger);
+  webserver.use(frontend);
 
+  webserver.get("/", function(req, res) {
+    res.json({ castWebApi: `v${configuration.thisVersion}` });
+  });
 
-	webserver.get('/', function (req, res) {
-		res.json({castWebApi: `v${configuration.thisVersion}`});
-	});
+  webserver.listen(configuration.port, () => {
+    console.log(
+      `cast-web-api running at http://${configuration.hostname}:${configuration.port}`
+    );
+  });
 
-	webserver.listen(configuration.port, () => {
-		console.log(`cast-web-api running at http://${configuration.hostname}:${configuration.port}`);
-	});
-
-	process.on('message', packet => {
-		console.log(configuration.hostname);
-		process.send({
-			type : 'process:msg',
-			data : {
-				address : `${configuration.hostname}:${configuration.port}`
-			}
-		});
-	});
+  process.on("message", packet => {
+    console.log(configuration.hostname);
+    process.send({
+      type: "process:msg",
+      data: {
+        address: `${configuration.hostname}:${configuration.port}`
+      }
+    });
+  });
 }
